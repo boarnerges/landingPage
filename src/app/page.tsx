bd01e1045acafcd0fc401bc5acf8c12aa05c41f3
boarnerges <olujawo1996@gmail.com>
2024-11-18 22:03:11 +0100

    Logo section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/sections/Hero";
+import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
 export default function Home() {
     return (
         <>
             <Navbar />
             <Hero />
+            <LogoTicker />
         </>
     );
 }
