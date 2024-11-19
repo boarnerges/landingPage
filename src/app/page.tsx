fdff121f207f1eab14e12d934ea440212991cce7
boarnerges <olujawo1996@gmail.com>
2024-11-19 08:38:03 +0100

    Introduction session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/sections/Hero";
+import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
             <Navbar />
             <Hero />
             <LogoTicker />
+            <Introduction />
         </>
     );
 }
