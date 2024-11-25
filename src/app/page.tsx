843b75f7b8469c6241ba923dfeb8331ae9fff43a
boarnerges <olujawo1996@gmail.com>
2024-11-25 13:44:49 +0100

    Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CallToAction from "@/sections/CallToAction";
 import Faqs from "@/sections/Faqs";
 import Features from "@/sections/Features";
+import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
@@ -17,6 +18,7 @@ export default function Home() {
             <Integrations />
             <Faqs />
             <CallToAction />
+            <Footer />
         </>
     );
 }
