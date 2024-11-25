55e4b199da97e5c24aecb419bdf807bcdcb7effb
boarnerges <olujawo1996@gmail.com>
2024-11-25 12:26:00 +0100

    Faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Faqs from "@/sections/Faqs";
 import Features from "@/sections/Features";
 import Hero from "@/sections/Hero";
+import Integrations from "@/sections/Integrations";
 import Introduction from "@/sections/Introduction";
 import LogoTicker from "@/sections/LogoTicker";
 import Navbar from "@/sections/Navbar";
@@ -11,6 +13,8 @@ export default function Home() {
             <LogoTicker />
             <Introduction />
             <Features />
+            <Integrations />
+            <Faqs />
         </>
     );
 }
